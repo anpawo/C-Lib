52a44fcccff78d8a3b168d84ab66f22b6d4f5c23
anpawo <112256146+anpawo@users.noreply.github.com>
2023-07-22 21:08:28 +0200

    Update mem_cpy.c

--- lib/my_utils/mem_cpy.c
+++ lib/my_utils/mem_cpy.c
@@ -16,11 +16,8 @@ void * mem_cpy(void * dst, const void * src, size_t n)
     if (n >= 8) {
         n -= dstp % 8;
         BYTE_COPY(dstp, srcp, dstp % 8);
-
         WORD_COPY(dstp, srcp, n);
     }
-
     BYTE_COPY(dstp, srcp, n);
-
     return dst;
 }
